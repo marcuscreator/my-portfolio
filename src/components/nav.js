ed28daf73b77714e339f6e727346566b49caa604
marcuscreator <kariukimacharia71@gmail.com>
2024-08-26 05:24:04 +0300

    add event listener to close the mobile nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,32 @@
-// components/Nav.js
 "use client";
-import React, { useState, useRef } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
   const fireworksContainerRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="sticky top-0 w-full p-4 flex justify-between items-center z-10 bg-black bg-opacity-70">
@@ -19,7 +35,11 @@ export default function Nav() {
       </div>
 
       <div className="lg:hidden">
-        <button onClick={toggleMenu} className="text-white focus:outline-none">
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none"
+          ref={buttonRef}
+        >
           <svg
             className="w-8 h-8"
             fill="none"
@@ -38,9 +58,10 @@ export default function Nav() {
       </div>
 
       <ul
+        ref={menuRef}
         className={`${
           isOpen ? "block" : "hidden"
-        } lg:flex lg:space-x-8 text-white font-bold bg-black bg-opacity-90 lg:bg-transparent lg:static absolute top-16 left-0 w-full lg:w-auto lg:top-0 lg:left-auto p-4 lg:p-0`}
+        } lg:flex lg:space-x-8 text-white font-bold bg-black bg-opacity-90 lg:bg-transparent lg:static absolute top-16 left-0 w-full lg:w-auto lg:top-0 lg:left-auto p-4 lg:p-0 transition-transform duration-300 ease-in-out`}
       >
         <li className="py-2 text-center lg:py-0">
           <a href="#home" className="block hover:underline">
@@ -72,7 +93,6 @@ export default function Nav() {
             href="/files/MARCUS_MACHARIA_CV.pdf"
             download
             className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-          
           >
             Download Resume
           </a>
@@ -84,7 +104,6 @@ export default function Nav() {
           href="/files/MARCUS_MACHARIA_CV.pdf"
           download
           className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-         
         >
           Download Resume
         </a>
